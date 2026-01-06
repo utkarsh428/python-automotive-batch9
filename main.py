ae0ee8dfecc888aef049a3cd38a84f716c1c77cf
Utkarsh Mishra <um21157@gmail.com>
2026-01-06 14:27:46 +0530

    commit 11

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from si import simple_interest
-
-#input from user
-P = int(input("Enter Principal amount: "))
-R = int(input("Enter Rate of interest: "))
-T = int(input("Enter Time: "))
-
-#calling function from si module
-final_result = simple_interest(P, R, T)
-
-#display the result
-print("Simple Interest is:", final_result)
+from api_client import fetch_users
+from validator import validate_response
+from file_handler import save_to_json, save_to_csv
+ 
+URL = "https://jsonplaceholder.typicode.com/users"
+ 
+def run():
+   response = fetch_users(URL)
+   users = validate_response(response)
+ 
+   print(f"API request successful: {response.status_code} OK")
+   print(f"Valid records processed: {len(users)}")
+ 
+   save_to_json("users.json", users)
+   save_to_csv("users.csv", users)
+ 
+   print("Data saved to users.json")
+   print("Data saved to users.csv")
+ 
+if __name__ == "__main__":
+   run()
